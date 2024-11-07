84ec3eb410626e9d29c053ca257c43fa9d55bf17
lizandro castillo <liiizandrocastillo@gmail.com>
2024-11-06 21:51:19 -0400

    Creacion y implementacion del widget ResponsiveContainer para diseño reutilizable

---
+++ lib/widgets/orientacion_responsive.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveContainer extends StatelessWidget {
+  final List<Widget> Function(BuildContext) buildBlocks;
+
+  const ResponsiveContainer({
+    Key? key,
+    required this.buildBlocks,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 63, 243, 180),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: OrientationBuilder(
+            builder: (context, orientation) {
+              // Detectar si el teléfono está en modo portrait o landscape
+              bool isPortrait = orientation == Orientation.portrait;
+
+              return isPortrait
+                  ? Column(
+                      children: buildBlocks(context),
+                    )
+                  : Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: buildBlocks(context),
+                    );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
